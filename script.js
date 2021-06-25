49e0b9ce428635ac615c828041eb8bbf4514a2d9
Caleb <calebstem@gmail.com>
2021-06-25 19:55:34 -0400

    Grid changes color when hovered over

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const toAdd = document.createDocumentFragment();
 const container = document.getElementById('container');
+const box = document.getElementsByClassName('box');
 
 for (let i = 1; i <= 256; i++){
-    console.log('working');
     const newDiv = document.createElement('div');
     newDiv.id = 'r'+i;
     newDiv.className = 'box';
+    newDiv.addEventListener('mouseenter', function(){
+        document.getElementById('r'+i).style.background = 'black' 
+    }, false);
     toAdd.appendChild(newDiv);
 }
+
+function changeColor(){
+    document.getElementById('r'+i).style.background = 'black';
+}
+
+//box.addEventListener('mouseenter', console.log('test'));
 
 container.appendChild(toAdd);
