736b1365372465b727dd2844e6968909aef5f343
Caleb <calebstem@gmail.com>
2021-06-25 22:55:40 -0400

    Implemented function newBox-removes previous box and creates a new one with set parameter

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ const toAdd = document.createDocumentFragment();
 const container = document.getElementById('container');
 const box = document.getElementsByClassName('box');
 
-let boxSize = 16;
-//let gridNum = 'repeat(boxSize, 26px)';
-
-container.style.gridTemplateColumns = `repeat(${boxSize}, 26px)`;
+let boxSize = 0;
 
 
-
-for (let i = 1; i <= (boxSize * boxSize); i++){
-    const newDiv = document.createElement('div');
-    newDiv.id = 'r'+i;
-    newDiv.className = 'box';
-    newDiv.addEventListener('mouseenter', function(){
-        document.getElementById('r'+i).style.background = 'black' 
-    }, false);
-    toAdd.appendChild(newDiv);
+function createBox(boxSize){
+    container.style.gridTemplateColumns = `repeat(${boxSize}, 26px)`;
+    for (let i = 1; i <= (boxSize * boxSize); i++){
+        const newDiv = document.createElement('div');
+        newDiv.id = 'r'+i;
+        newDiv.className = 'box';
+        newDiv.addEventListener('mouseenter', function(){
+            document.getElementById('r'+i).style.background = 'black' 
+        }, false);
+        toAdd.appendChild(newDiv);
+    }
+    container.appendChild(toAdd);
 }
 
+function removeBox(){
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
 
-container.appendChild(toAdd);
+function newBox(){
+    removeBox();
+    boxSize = prompt('Box Size');
+    createBox(boxSize);
+}
